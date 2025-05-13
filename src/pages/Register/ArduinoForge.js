162f58471e5bfddcdfeba3a88d4e23bdd7251b19
Aishwaryaacharya-2004 <125909387+Aishwaryaacharya-2004@users.noreply.github.com>
2025-05-13 18:21:37 +0530

    Update ArduinoForge.js

--- src/pages/Register/ArduinoForge.js
+++ src/pages/Register/ArduinoForge.js
@@ -92,12 +92,12 @@ const Register = () => {
   if (clicked) {
     return (
       <div style={{ display: iframe ? 'block' : 'none' }}>
-        <iframe
+       <iframe
           onLoad={() => setIframe(true)}
           width="100%"
           height="100%"
           style={{ border: 'none', minHeight: "100vh" }}
-          src="https://www.yepdesk.com/embed/buy-tickets/681e558ac9e77c0001e55031/private/t3t53lq2ip"
+          src="https://www.yepdesk.com/embed/buy-tickets/682338c4c9e77c0001f170ba/private/ukmvh2dqm3"
         />
       </div>
     );
