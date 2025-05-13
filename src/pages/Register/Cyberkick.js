f526552c17b8dae844b940dbf907e7c4db92cd06
Aishwaryaacharya-2004 <125909387+Aishwaryaacharya-2004@users.noreply.github.com>
2025-05-13 18:27:03 +0530

    Update Cyberkick.js

--- src/pages/Register/Cyberkick.js
+++ src/pages/Register/Cyberkick.js
@@ -92,12 +92,12 @@ const Register = () => {
   if (clicked) {
     return (
       <div style={{ display: iframe ? 'block' : 'none' }}>
-        <iframe
+       <iframe
           onLoad={() => setIframe(true)}
           width="100%"
           height="100%"
           style={{ border: 'none', minHeight: "100vh" }}
-          src="https://www.yepdesk.com/embed/buy-tickets/681e558ac9e77c0001e55031/private/t3t53lq2ip"
+          src="https://www.yepdesk.com/embed/buy-tickets/68233cbac9e77c0001f17764/private/8mco8gri6i"
         />
       </div>
     );
