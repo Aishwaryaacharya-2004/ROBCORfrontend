c03960f9525ac0b0cca18b3efb7061bb717807f1
Aishwaryaacharya-2004 <125909387+Aishwaryaacharya-2004@users.noreply.github.com>
2025-05-13 18:29:36 +0530

    Update Neonrun.js

--- src/pages/Register/Neonrun.js
+++ src/pages/Register/Neonrun.js
@@ -97,7 +97,7 @@ const Register = () => {
           width="100%"
           height="100%"
           style={{ border: 'none', minHeight: "100vh" }}
-          src="https://www.yepdesk.com/embed/buy-tickets/681e558ac9e77c0001e55031/private/t3t53lq2ip"
+         src="https://www.yepdesk.com/embed/buy-tickets/68233c22c9e77c0001f176d9/private/ibb7lpvvb9"
         />
       </div>
     );
