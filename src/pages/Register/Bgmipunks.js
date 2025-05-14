86d20d27149218f6103bc7b0637669c8df590570
Aishwaryaacharya-2004 <125909387+Aishwaryaacharya-2004@users.noreply.github.com>
2025-05-14 11:09:06 +0530

    Update Bgmipunks.js

--- src/pages/Register/Bgmipunks.js
+++ src/pages/Register/Bgmipunks.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Form, Button, Alert, Spinner } from "react-bootstrap";
 import axios from "axios";
 import './register.css';
-import starlink from '../../assets/compressed/starlink.jpg';
+//import starlink from '../../assets/compressed/starlink.jpg';
 import rulebook from '../../assets/rulebook.pdf';
 import character7 from '../../assets/compressed/charater7.jpg';
 import backgroundImg from '../../assets/demo.jpg';
